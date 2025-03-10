f94ffec3ea0e1b6ce616f0faa1740c646fc312b7
Parisotto <parisotto@gmail.com>
2025-03-10 09:50:42 -0300

    exercíxios

---
+++ base02.py
@@ -0,0 +1,61 @@
+# 1. Adivinhação de números:
+#  • Crie uma lista com 10 números.
+#  • Peça ao usuário para adivinhar um número da lista.
+#  • Use um loop while para continuar pedindo adivinhações até que o usuário acerte.
+
+
+# 2. Contagem regressiva:
+#  • Crie uma lista de contagem regressiva de 10 a 1.
+#  • Use um loop while para imprimir cada número da lista.
+
+
+# 3. Adição de números:
+#  • Crie uma lista vazia para armazenar números.
+#  • Peça ao usuário para fornecer números e os adicionem à lista.
+#  • Continue pedindo números até que o usuário decida parar.
+
+
+# 4. Média de notas:
+#  • Crie uma lista vazia para armazenar notas.
+#  • Peça ao usuário para fornecer notas e as adicionem à lista.
+#  • Calcule e imprima a média das notas quando o usuário decidir parar.
+
+
+# 5. Busca em lista:
+#  • Crie uma lista de cinco nomes.
+#  • Peça ao usuário para digitar um nome.
+#  • Usemum loop while para verificar se o nome está na lista e informar o resultado.
+
+
+# 6. Contador de números:
+#  • Solicite ao usuário um número inicial.
+#  • Use um loop while para imprimir os números de 1 até o número fornecido pelo usuário.
+#  • Exiba uma mensagem indicando que o loop terminou.
+
+
+# 7. Média de idades
+#  • Crie um programa que peça ao usuário que insira idades de pessoas até que ele digite um valor negativo.
+#  • Em seguida, utilize um loop while para calcular e exibir a média das idades inseridas.
+
+
+# 8. Números ímpares
+#  • Com a lista numeros = [10, 15, 7, 23, 42, 5, 17, 8, 31, 12, 9, 25, 3, 19, 16, 28, 36, 14, 21, 6], exiba apenas os números ímpares da lista.
+
+
+# 9. Números primos
+#  • Crie um programa que solicite ao usuário que insira números inteiros positivos até que ele digite zero. 
+#  • Use um loop while para determinar quantos números primos foram inseridos.
+
+
+# 10. Remoção de itens
+#  • Com a lista nomes = [“Maria”, “José”, “Ana”, “Pedro”, “João”, “Luiza”, “Rafael”, “João”, “Carolina”, “Fernando”], remova todos os nomes “João” da lista e exibam a lista resultante.
+
+
+# 11. Calcular
+#  • Crie um programa que solicite ao usuário que insira alturas (por exemplo, 1,80 m) de pessoas até que ele digite um valor negativo. 
+#  • Calcule a média e identifiquem a maior e a menor altura.
+
+
+# 12. Fatorial
+#  • Crie um programa que solicite ao usuário que insira um número inteiro positivo. 
+#  • use um loop while para calcular e exibir o fatorial desse número.
