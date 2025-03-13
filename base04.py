a98f34f6606dcd2dfe7307e6bcdbbfc243ee1612
Parisotto <parisotto@gmail.com>
2025-03-13 10:31:04 -0300

    Aula 13/03/2025

--- base04.py
+++ base04.py
@@ -4,17 +4,18 @@ import random
 
 def numeros_pares():
   pares = []
-  quantidade = int(input("Digite quantos número pares deve ter sua lista: "))
+  try: 
+    quantidade = int(input("Digite quantos número pares deve ter sua lista: "))
+  except ValueError:
+    print("ERRO! Digite apenas um número inteiro!")
+    exit()
   contador = 1
   while contador <= quantidade:
-    try:
       aleatorio = random.randint(1, 1000)
       if aleatorio % 2 == 0:
         pares.append(aleatorio)
       contador += 1
-    except ValueError:
-      print("ERRO! Digite apenas um número inteiro!")
-      
+
   print(pares)
 
 numeros_pares()
