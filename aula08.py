0e492916f61443694bb50c9d591730311d82e57f
Parisotto <parisotto@gmail.com>
2025-03-18 09:01:37 -0300

    Aula 18/03/2025

---
+++ aula08.py
@@ -0,0 +1,19 @@
+# Exercícios
+
+#  1. Escreva uma função que retorne o maior de dois números.
+
+#  2. Escreva uma função que receba dois números e retorne True, se o primeiro número for múltiplo do segundo.
+
+#  3. Escreva uma função que receba o lado (l) de um quadrado e retorne sua área (A = lado2).
+
+#  4. Escreva uma função que receba a base e a altura de um triângulo e retorne sua área (A = (base x altura)/2).
+
+#  5. Faça uma função que recebe um nome e imprime “olá, [nome]”.
+
+#  6. Faça uma função que recebe um nome e um horário e imprime “Bom dia, [nome]”, caso seja antes de 12h; “Boa Tarde, [nome]”, caso seja entre 12h e 18h; e “Boa noite, [nome]” caso seja após às 18h.
+
+#  7. Faça uma função que recebe um número e retorna True, se ele for par, ou False, se ele for ímpar.
+
+#  8. Escreva uma função para validar uma variável string. Essa função recebe como parâmetro a string, os números mínimo e máximo de caracteres. Retorne verdadeiro,se o tamanho da string estiver entre os valores de máximo e mínimo, e falso em caso contrário.
+
+#  9. Escreva uma função que receba uma string e uma lista. A função deve comparar a string passada com os elementos da lista, também passada como parâmetro. Retorne verdadeiro, se a string for encontrada dentro da lista, e falso, em caso contrário.
