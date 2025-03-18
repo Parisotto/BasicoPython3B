cf07982f0592be27a0be40a30a3d5153d8328e95
Parisotto <parisotto@gmail.com>
2025-03-18 10:08:55 -0300

    aula 18/03/2025

--- base07.py
+++ base07.py
@@ -1,1 +1,39 @@
-# FUNCÕES
+# FUNCÕES def
+
+def minhaFuncao():
+  lista = list(range(1, 11))
+  print(len(lista))
+
+minhaFuncao()
+
+def outraFuncao(parametro):
+  print(parametro)
+
+outraFuncao(9)
+outraFuncao("Texto")
+lista = list(range(1, 11))
+outraFuncao(lista)
+
+def novaFuncao(p1, p2):
+  print(p1 + p2)
+
+novaFuncao(9, 7)
+
+def outraFuncao(p1, p2):
+  print(p1 * p2)
+
+def outraFuncao():
+  print("Olá, mundo!")
+
+def somaLista(lista):
+  return sum(lista)
+
+print(somaLista(lista))
+
+def somaInteiros(n1 :int, n2: int):
+  if not isinstance(n1, int) or not isinstance(n2, int):
+    return "Erro!"
+  else:
+    return n1 + n2
+
+print(somaInteiros("d", 3))
