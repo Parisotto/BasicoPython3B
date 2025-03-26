bbdbea19743fe2a282d8e7672d4c3d475c75d152
Parisotto <parisotto@gmail.com>
2025-03-26 07:20:31 -0300

    Aula 26/03/2025

---
+++ base12.py
@@ -0,0 +1,24 @@
+# MEGASENA
+
+# Faça uma função que retorne números aleatórios para a Megasena.
+# O usuário deverá escolher quantos números irá apostar, sendo no mínimo 6 e no máximo 20 números de 1 a 60.
+# Lembre-se de que a lista final de escolhas não pode conter um valor repetido. 
+# Permita que o usuário continue fazendo outros jogos da Megasena até que ele digite 0 (zero) para parar. 
+# Ao final, mostre quanto o usário deverá pagar conforme o número de jogos e de palpites por jogo, conforme os valores abaixo:
+# Números Escolhidos	Valor da Aposta
+# 6	                  R$ 5,00
+# 7	                  R$ 35,00
+# 8	                  R$ 140,00
+# 9	                  R$ 420,00
+# 10	                R$ 1.050,00
+# 11	                R$ 2.310,00
+# 12	                R$ 4.620,00
+# 13	                R$ 8.580,00
+# 14	                R$ 15.015,00
+# 15	                R$ 25.025,00
+# 16	                R$ 40.040,00
+# 17	                R$ 61.880,00
+# 18	                R$ 92.820,00
+# 19	                R$ 135.660,00
+# 20	                R$ 193.800,00
+
