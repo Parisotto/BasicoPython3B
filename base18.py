5d86c915c4ed13cf0f0bfd866244fbd55c0b7c73
Parisotto <parisotto@gmail.com>
2025-04-08 10:12:58 -0300

    Aula 08/04/2025

---
+++ base18.py
@@ -0,0 +1,23 @@
+# LISTAS, TUPLAS, CONJUNTOS E DICIONÁRIO -> Aninhados
+
+nomes = ["Sofia", "Abgail", "Ines"]
+cores = ["Amarelo", "Azul", "Vermelho", "Verde"]
+
+lista_aninhada = [nomes, cores]
+
+print(nomes[2])
+print(cores[3])
+print(lista_aninhada[1][1])
+print(lista_aninhada[0][2])
+
+nomes = ("Sofia", "Cristal", "Matheus", "Guilherme")
+cores = ("Vermelho", "Azul", "Verde")
+
+tupla = (nomes, cores)
+
+print(tupla)
+
+print(nomes[2])
+print(tupla[0])
+print(tupla[0][2])
+
