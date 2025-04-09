6df04b5f47f47de641056cb4683e0fd0510d1bff
Parisotto <parisotto@gmail.com>
2025-04-09 07:54:10 -0300

    aGENDA 09/04/2025

---
+++ base19.py
@@ -0,0 +1,24 @@
+# TRABALHO: Projeto: Agenda de contatos
+
+# Desenvolva um programa em Python para gerenciar uma agenda de contatos, que deve oferecer as funcionalidades listadas a seguir:
+
+# 1 Adicionar contato: permitir que o usuário adicione novos contatos, fornecendo nome, idade, telefone, e-mail, renda e estado.
+
+# 2 Exibir contatos: mostrar uma lista completa de todos os contatos na agenda, incluindo nome, telefone e e-mail.
+
+# 3 Buscar contato: permitir que o usuário busque as informações de um contato específico, fornecendo o nome do contato.
+
+# 4 Remover contato: possibilitar a remoção de um contato da agenda com base no nome.
+
+# 5 Corrigir contato: substituir uma informação de um contato.
+
+# 6 Mostrar a quantidade de contatos cadastrados.
+
+# 7 Mostrar a média de idade dos contados cadastrados.
+
+# 8 Mostrar a quantidade de contatos cadastrados por estado
+
+'''
+Lembre-se de que o programa deve permitir que o usuário preencha os contatos até digitar “sair”
+'''
+
