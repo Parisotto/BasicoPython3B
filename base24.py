016bd0b29b451f0521e64e1da73afa533ccf5f09
Parisotto <parisotto@gmail.com>
2025-04-16 08:11:37 -0300

    Aula 16/04/2025

--- base24.py
+++ base24.py
@@ -1,15 +1,61 @@
 # EXERCÍCIO MATRIZ 3x3 (Tipo Jogo da Velha)
 
 # Crie uma matriz 3x3 com cada elemento prenchido com valores sequenciais de 1 a 9
+matriz = [[1,2,3],[4,5,6],[7,8,9]]
+marcador = "X"
 
-# Imprima cada linha da matriz em um linha diferente.
+while True:
+  # Imprima cada linha da matriz em um linha diferente.
+  print()
+  for linha in matriz:
+    for valor in linha:
+      print(f"[{valor:^3}]", end="")
+    print()
 
-# Solicite ao usário escolher uma das nove posições digitando o número correspondente
 
-# Verifique se o usuário digitou mesmo um número válido e dentro do intervalo de 1 a 9
+  # Solicite ao usário escolher uma das nove posições digitando o número correspondente
+  escolha = input("Digite um número da matriz disponível: ")
 
-# Substitua o valor escolhido por X (letra X maiúscula) ou por O (letra O maiúscula) alternadamente começando por X
-      
-# Se a casa escolhida pelo usuário já tiver sido preenchida antes por X ou O, imprima uma mensagem informando e solicite novamente outra opção válida.
+  # Verifique se o usuário digitou mesmo um número válido e dentro do intervalo de 1 a 9
+  if escolha.isnumeric():
+    escolha = int(escolha)
+    if escolha >= 1 and escolha <= 9:
+  # Substitua o valor escolhido por X (letra X maiúscula) ou por O (letra O maiúscula) alternadamente começando por X
+      feito = False
+      for linha in range(3):
+        for coluna in range(3):
+          if matriz[linha][coluna] == escolha:
+            matriz[linha][coluna] = marcador
+            feito = True
+            if marcador == "X":
+              marcador = "O"
+            else:
+              marcador = "X"
+            break
+        if feito:
+          break
 
-# Quando todas as 9 casas da matriz tiver sido preenchidas, avise o usuário e termine o programa.
+  # Se a casa escolhida pelo usuário já tiver sido preenchida antes por X ou O, imprima uma mensagem informando e solicite novamente outra opção válida.
+      if feito == False:
+        print("\nEsta casa já fo escolhida. Escolha outra opção válida.")
+
+# Quando todas as 9 casas da matriz já tiver sido preenchidas, avise o usuário e termine o programa.
+  continuar = False
+  for linha in matriz:
+    for valor in linha:
+      if valor in list(range(1,10)):
+        continuar = True
+        break
+    if continuar:
+      break
+
+  if continuar == False:
+    print()
+    for linha in matriz:
+      for valor in linha:
+        print(f"[{valor:^3}]", end="")
+      print()
+
+    print("Todas as casas já foram preenchidas!")
+    print("Fim do programa!\n")
+    break
