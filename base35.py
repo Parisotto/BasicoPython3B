5c138fb7430808a79119de7a57ff757982b79402
Parisotto <parisotto@gmail.com>
2025-04-16 08:20:44 -0300

    Aula 16/05/2025

---
+++ base35.py
@@ -0,0 +1,4 @@
+# JOGO DA VELHA
+
+# Com base no exercício anterior, crie um JOGO DA VELHA completo. Quando um dos jogadores fizer a velha, informe quem foi o vencedor, se X ou O, e encerre o programa.
+
