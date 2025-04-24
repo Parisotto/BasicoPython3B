8fb80a1807c4cb8ee3cfce27fa7576e84f80e80e
Parisotto <parisotto@gmail.com>
2025-04-24 17:36:42 -0300

    Aula 24/04/2025

--- base25.py
+++ base25.py
@@ -1,4 +1,27 @@
-# JOGO DA VELHA
+import tkinter as tk
 
-# Com base no exercício anterior, crie um JOGO DA VELHA completo. Quando um dos jogadores fizer a velha, informe quem foi o vencedor, se X ou O, e encerre o programa.
+janela = None
 
+def interface():
+  rotulo = tk.Label(janela, text="Tarefa")
+  rotulo.pack(pady=10)
+
+  tarefa = tk.Entry(janela, width=70)
+  tarefa.pack(pady=10, padx=20)
+
+  bt_adicionar = tk.Button(janela, text="Adicionar Tarefa")
+  bt_adicionar.pack(pady=10)
+
+  lista = tk.Listbox(janela, width=70, height=10)
+  lista.pack(padx=20, pady=10)
+
+  bt_concluir = tk.Button(janela, text='Concluir')
+  bt_concluir.pack(pady=10)
+
+def main():
+  janela = tk.Tk()
+  janela.title("Gerenciador de Tarefas")
+  interface()
+  janela.mainloop()
+
+main()
